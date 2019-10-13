bbaf599b78fabdda8c8ab1088fd37f4c75056a2f
Ilya Leanovich <leanovich.ilya@gmail.com>
2019-10-13 15:29:08 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+
+  str = str.split('');
+
+  const template = bracketsConfig
+    .toString()
+    .split(',')
+    .join('');
+  let count = 0;
+
+  for (let i = 0; i < template.length; i += 2) {
+    for (let j = 0; j < str.length; j += 1) {
+      if (str[j] === template[i]) {
+        count += 1;
+        for (let k = j + 1; k < str.length; k += 1) {
+          if ((k - j - 1) % 2 === 0 && template[i + 1] === str[k]) {
+            count -= 1;
+            str[k] = '';
+            break;
+          }
+        }
+      }
+
+      if (count < 0) {
+        return false;
+      }
+    }
+
+    if (count !== 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
